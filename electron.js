846c1f5ca1d11a95545fb8d57dcb02a299b0efd5
htjb <harrytjbevins@yahoo.co.uk>
2025-11-01 15:33:59 +0000

    electron files

---
+++ electron.js
@@ -0,0 +1,24 @@
+import {app, BrowserWindow} from 'electron';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.loadURL('http://localhost:5173');
+    } else {
+        mainWindow.loadFile(path.join(__dirname, 'dist/index.html'));
+    }
+}
+
+app.whenReady().then(createWindow);
